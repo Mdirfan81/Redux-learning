896ecc8f9ce16a809a9f24da63dae8d3698fabc1
mdirfukhan <mdirfankhan98455@gmail.com>
2022-03-29 14:53:14 +0530

    added the bucket list and added CSS

--- src/containers/SideDisplayContainer.js
+++ src/containers/SideDisplayContainer.js
@@ -1,10 +1,13 @@
 import { connect } from "react-redux";
 import SideDisplay from "../components/SideDisplay";
+import { addToCart, removeToCart } from "../services/actions/action";
 
 const mapStateToProps = (state) => ({
   data: state.cardItems,
 });
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  removeCardHandler: (data) => dispatch(removeToCart(data)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(SideDisplay);
